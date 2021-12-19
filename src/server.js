e76b89fa88b6c3ceda801032876651ad1b06e250
Yena-Yun <cheryleduart@gmail.com>
2021-12-19 21:11:13 +0900

    feat: 프론트엔드 pug, 백엔드 server 설정

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
+// 이 프로젝트에서 Express가 하는 일은 매우 간단: pug로 만들어진 views를 설정 후 3000번 서버에 렌더하는 역할뿐 (나머지는 websocket에서 실시간으로 일어남)
 import express from 'express';
 
 const app = express();
 
-console.log('hello');
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views'); // __dirname = 'src'
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (req, res) => res.render('home'));
+app.get('/*', (req, res) => res.redirect('/'));
 
-app.listen(3000);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);
