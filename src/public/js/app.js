b54b948efc705ede28c3e55a9877b426939f1cd8
Yena-Yun <cheryleduart@gmail.com>
2021-12-20 21:33:06 +0900

    feat: 서버와 프론트엔드 메시지 주고받기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,5 +1,24 @@
-// var aWebSocket = new WebSocket(url [, protocols]);
-const socket = new WebSocket(`ws://${window.location.host}`); // url에서 http 부분을 ws로 대체하고 url에는 현재 위치를 의미하는 location.host를 넣어준다 (그냥 localhost:3000 넣으면 모바일에서 못씀)
-
+// MDN: var aWebSocket = new WebSocket(url [, protocols]);
 // app.js의 socket: 서버로의 연결(Backend와 요청 및 응답을 주고받기 위한 소켓)
-console.log(socket);
+const socket = new WebSocket(`ws://${window.location.host}`);
+// url에서 http 부분을 ws로 대체 + 현재 위치를 의미하는 location.host를 넣어줌 (localhost:3000 넣으면 웹만 해당되어서 모바일은 못씀)
+// (브라우저 console에 window.location 치면 location 객체 볼 수 있음)
+
+// socket이 open되면 서버에 연결됨
+socket.addEventListener('open', () => {
+  console.log('browser connected to server!');
+});
+
+socket.addEventListener('message', (message) => {
+  console.log('New message:', message.data);
+});
+
+socket.addEventListener('close', () => {
+  console.log('disconnected from server');
+});
+
+// 시간 지연을 주기 위해 setTimeout 사용
+// 10초 뒤에 서버에게 메시지를 보냄
+setTimeout(() => {
+  socket.send('hello from the browser');
+}, 5000);
--- src/server.js
+++ src/server.js
@@ -14,15 +14,27 @@ app.get('/*', (req, res) => res.redirect('/'));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 // app.listen(3000, handleListen);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const server = http.createServer(app); // http 서버 (pug 뷰, 라우터)
+const wss = new WebSocket.Server({ server }); // 웹소켓 서버 (실시간 통신)
+// 둘 중에 하나만 써도 되는데 이 프로젝트에서는 하나의 포트 안에 http와 웹소켓을 모두 처리하고 있어서 두 개를 한번에 사용
 
-const handleConnection = (socket) => {
-  // server.js의 socket = 연결된 브라우저(클라이언트)
-  // Frontend와 요청 및 응답을 주고받기 위한 소켓
-  console.log(socket);
-};
+// server.js의 socket = 연결된 브라우저(클라이언트)
+// Frontend와 요청 및 응답을 주고받기 위한 소켓
+wss.on('connection', (socket) => {
+  // 브라우저에 연결되자마자 콘솔 출력
+  console.log('connected to Browser');
 
-wss.on('connection', handleConnection);
+  // on = Listener
+  // 서버가 끊겼을 때를 대비한 Listener
+  socket.on('close', () => console.log('disconnected from the Browser'));
+
+  // 서버가 메시지를 보낼 때를 위한 Listener
+  socket.on('message', (message) => {
+    console.log(message.toString('utf8'));
+  });
+
+  // send: 실제 브라우저에 메시지를 보냄
+  socket.send('hello!!');
+});
 
 server.listen(3000, handleListen);
