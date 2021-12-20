e2f9806953f606a014ddd21db90d011eee45f22d
Yena-Yun <cheryleduart@gmail.com>
2021-12-20 12:52:20 +0900

    feat: 프론트엔드와 백엔드 파일에 각각 웹소켓 연결

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,5 @@
-alert('hi!');
+// var aWebSocket = new WebSocket(url [, protocols]);
+const socket = new WebSocket(`ws://${window.location.host}`); // url에서 http 부분을 ws로 대체하고 url에는 현재 위치를 의미하는 location.host를 넣어준다 (그냥 localhost:3000 넣으면 모바일에서 못씀)
+
+// app.js의 socket: 서버로의 연결(Backend와 요청 및 응답을 주고받기 위한 소켓)
+console.log(socket);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 // 이 프로젝트에서 Express가 하는 일은 매우 간단: pug로 만들어진 views를 설정 후 3000번 서버에 렌더하는 역할뿐 (나머지는 websocket에서 실시간으로 일어남)
+import http from 'http';
+import WebSocket from 'ws';
 import express from 'express';
 
 const app = express();
@@ -10,4 +12,17 @@ app.get('/', (req, res) => res.render('home'));
 app.get('/*', (req, res) => res.redirect('/'));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+// app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const handleConnection = (socket) => {
+  // server.js의 socket = 연결된 브라우저(클라이언트)
+  // Frontend와 요청 및 응답을 주고받기 위한 소켓
+  console.log(socket);
+};
+
+wss.on('connection', handleConnection);
+
+server.listen(3000, handleListen);
